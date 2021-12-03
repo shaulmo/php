2d6006cf6a522d7a769471b67fa0c092af6cde22
shaulmo <95495331+shaulmo@users.noreply.github.com>
2021-12-03 10:09:36 -0800

    Create power.php

---
+++ power.php
@@ -0,0 +1,11 @@
+// Creating PHP Power function from scratch for learning purposes.
+
+function power($base, $exponent){
+    $result = 1;
+    for($i=$exponent; $i > 0; $i--){
+        $result = $result * $base;
+    }
+    return $result;
+}
+
+// Example: echo power(5,2);
